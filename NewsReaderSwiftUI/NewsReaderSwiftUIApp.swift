c67de60f85818616925491d19e30e7e40b533f6d
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-07-16 16:33:26 +0200

    Initial Commit

---
+++ NewsReaderSwiftUI/NewsReaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderSwiftUIApp.swift
+//  NewsReaderSwiftUI
+//
+//  Created by Tomislav Gelesic on 16.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
